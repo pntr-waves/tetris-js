f342557988ccd14a88a0b96beb4f1e6ec976b8bb
Evolus <evolus@localhost.localdomain>
2021-05-14 16:58:40 +0700

    UI board game

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,7 @@ for(let i=0;i<200;i++)
   let block='<div class="block" ></div>'
   board.innerHTML += block
 }
+
+falls = () => {
+  let 
+}
---
+++ static/js/game.js
@@ -0,0 +1,29 @@
+let btns = document.querySelectorAll('[id*="btn-"]');
+
+btns.forEach((e) => {
+  let btn_id = e.getAttribute("id");
+  let body = document.querySelector("body");
+  e.addEventListener("click", () => {
+    switch (btn_id) {
+      case "btn-play":
+        body.classList.add("play");
+        if (body.classList.contains("pause")) {
+          body.classList.remove("pause");
+        }
+        break;
+      case "btn-theme":
+        body.classList.toggle("dark");
+        break;
+      case "btn-pause":
+        let btnplay = document.querySelector("#btn-play");
+        btnplay.innerHTML = 'Resume'
+        body.classList.remove("play");
+        body.classList.add("pause");
+        break;
+      case "btn-new-game":
+        body.classList.add("play");
+        body.classList.remove("pause");
+        break;
+    }
+  });
+});
