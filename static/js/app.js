4244152af066282159e53f1baecbb376f045f6de
Evolus <evolus@localhost.localdomain>
2021-05-17 17:26:50 +0700

    loading effect 50%

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,37 @@
-let board = document.querySelector('.board-section')
-for(let i=0;i<200;i++)
-{
-  let block='<div class="block" ></div>'
-  board.innerHTML += block
+let board = document.querySelector(".board-section");
+for (let i = 0; i < 200; i++) {
+  let block = '<div class="block" ></div>';
+  board.innerHTML += block;
 }
 
 falls = () => {
-  let 
+  let fall_count = 50;
+  let container = document.querySelector(".container");
+  let i = 0;
+
+  for (i; i < fall_count; i++) {
+    let img_index = Math.floor(Math.random() * 6) + 1;
+    let x = Math.floor(Math.random() * window.innerWidth);
+    let y = Math.floor(Math.random() * window.innerHeight);
+    let size = Math.random() * 40;
+    let duration = Math.random() * 70 + 30;
+    let img = document.createElement("img");
+    img.className = "fall";
+    img.src = "static/images/" + img_index + ".png";
+    img.style.width = 1 + size + "px";
+    img.style.height = "auto";
+    img.style.left = x + "px";
+    img.style.bottom = y + "px";
+    img.style.animationDuration = 1 + duration + "s";
+    img.style.animationDelay = -duration + "s";
+    container.appendChild(img);
+  }
+};
+
+falls();
+
+let loading_square = document.querySelector('.square');
+
+for(let i=0;i<16; i++){
+  loading_square.innerHTML += '<div></div>'
 }
