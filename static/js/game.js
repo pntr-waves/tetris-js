8eb7ce787bca1d64698c38f6a0675aaa4a898b62
Evolus <evolus@localhost.localdomain>
2021-05-18 16:07:54 +0700

    uncomplete moveable

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,93 @@
+let field = document.getElementsByClassName("block");
+
+newGrid = (width, height) => {
+  let grid = new Array(height);
+  for (let i = 0; i < height; i++) {
+    grid[i] = new Array(width);
+  }
+
+  let index = 0;
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      grid[i][j] = {
+        index: index++,
+        value: 0,
+      };
+    }
+  }
+  return {
+    board: grid,
+    width: width,
+    height: height,
+  };
+};
+
+resetGrid = (grid) => {
+  for (let i = 0; i < grid.height; i++) {
+    for (let j = 0; i < grid.width; j++) {
+      grid.board[i][j].value = 0;
+    }
+  }
+
+  Array.from(field).forEach((e) => {
+    e.style.background = TRANSPARENT;
+  });
+};
+
+newTetromino = (blocks, colors, start_x, start_y) => {
+  let index = Math.floor(Math.random() * blocks.length);
+  return {
+    block: JSON.parse(JSON.stringify(blocks[index])),
+    color: COLORS[index],
+    x: start_x,
+    y: start_y,
+  };
+};
+
+drawTetromino = (tetromino, gird) => {
+  tetromino.block.forEach((row, i) => {
+    row.forEach((value, j) => {
+      let x = tetromino.x + i;
+      let y = tetromino.y + j;
+      if (value > 0) {
+        field[grid.board[x][y].index].style.background = tetromino.color;
+      }
+    });
+  });
+};
+
+clearTetromino = (tetromino, grid) => {
+  tetromino.block.forEach((row, i) => {
+    row.forEach((value, j) => {
+      let x = tetromino.x + i;
+      let y = tetromino.y + j;
+      if (value > 0) {
+        field[grid.board[x][y].index].style.background = TRANSPARENT;
+      }
+    });
+  });
+};
+
+isInGrid = (x, y, grid) => {
+  return x < grid.height && (x >= 0) & (y >= 0) & (y < grid.width);
+};
+
+isFilled = (x,y,grid)=>{
+  if()
+}
+
+moveDown = (tetromino, grid) => {
+  clearTetromino(tetromino, grid);
+  tetromino.x++;
+  drawTetromino(tetromino, grid);
+};
+
+let grid = newGrid(GRID_WIDTH, GRID_HEIGHT);
+
+let tetromino = newTetromino(BLOCK, COLORS, START_X, START_Y);
+
+drawTetromino(tetromino, grid);
+
 let btns = document.querySelectorAll('[id*="btn-"]');
 
 btns.forEach((e) => {
@@ -25,8 +115,8 @@ btns.forEach((e) => {
         body.classList.remove("pause");
         break;
       case "btn-help":
-        let how_to=document.querySelector('.how-to')
-        how_to.classList.toggle('active')
+        let how_to = document.querySelector(".how-to");
+        how_to.classList.toggle("active");
         break;
     }
   });
